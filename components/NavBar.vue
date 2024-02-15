3146d4925f4c89eeb226c7c8386f2b9da2e58397
Maxime Bignolet <maxime.bignolet@rhinos-groupe.com>
2024-02-15 23:14:20 +0100

    feat(): layout default + inte home page

---
+++ components/NavBar.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="h-20 flex justify-between items-center">
+    <div class="pl-5 flex items-center gap-3 py-5">
+      <img src="~/assets/image/logo.png" alt="" class="h-6 w-6" />
+      <p class="text-[#11BCC7] font-semibold">JUICEDAILY</p>
+    </div>
+    <div class="py-5">
+      <nav class="font-thin text-xl">
+        <NuxtLink to="#">Browse</NuxtLink>
+        <NuxtLink to="#" class="mx-20">Trending</NuxtLink>
+        <NuxtLink to="#">Contributors</NuxtLink>
+        <NuxtLink to="#" class="ml-20">Community</NuxtLink>
+      </nav>
+    </div>
+    <div class="bg-[#FF553C] h-full p-5 m-0 flex items-center cursor-pointer">
+      <FontAwesomeIcon
+        :icon="faMagnifyingGlass"
+        size="lg"
+        class="h-6 text-white font-bold"
+      />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { faMagnifyingGlass } from "@fortawesome/pro-light-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div
+    class="h-screen bg-[url('~/assets/image/bg-home.png')] bg-no-repeat bg-cover"
+  >
+    <div class="flex justify-end my-auto h-5/6">
+      <div class="flex flex-col justify-center">
+        <div
+          class="bg-[#4E9AA9]/15 w-[40%] flex gap-5 items-center p-3 cursor-pointer hover:scale-[1.1] transition-all ease-linear"
+        >
+          <p class="bg-white w-fit text-xs p-2 text-[#4E9AA9]">NEW RECIPE</p>
+          <p class="text-[#4E9AA9] text-sm">
+            Delicious fruit juice combinations
+          </p>
+          <FontAwesomeIcon :icon="faArrowRight" class="h-3 text-[#4E9AA9]" />
+        </div>
+        <div class="pt-5">
+          <h1 class="font-bold text-7xl w-[70%]">
+            Simple and Tasty Juice Recipes
+          </h1>
+          <p class="w-1/2 pt-5 text-gray-400">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae
+            deleniti non laudantium asperiores minus, quo commodi consequuntur
+            beatae eos nesciunt.
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { faArrowRight } from "@fortawesome/pro-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+</script>
